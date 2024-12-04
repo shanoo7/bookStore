af224f6f52146c096f83a50655306116562f2f05
shanoo <shanoo.kumar11@gmail.com>
2024-12-04 10:07:30 -0800

    adding components

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar"
+import Banner from "./components/Banner"
+import Footer from "./components/Footer"
+
+
+function App() {
+  //js
+
+  return (
+    <>
+      {/* jsx */}
+<Navbar/>
+<Banner/>
+<Footer/>
+    </>
+  )
+}
+
+export default App
