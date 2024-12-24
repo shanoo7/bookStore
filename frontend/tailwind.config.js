858c337cfb5aa6d0ee37d0c6484c14440788b815
shanoo <shanoo.kumar11@gmail.com>
2024-12-24 02:21:04 -0800

    add tailwind.config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode:"class",
+  darkMode: "class",
   theme: {
     extend: {},
   },
   plugins: [
-    require('daisyui'),
+    daisyui,
   ],
 }
