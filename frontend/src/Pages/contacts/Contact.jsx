18005b6d8b9eb217e8c63e730838899927b26b40
shanoo <shanoo.kumar11@gmail.com>
2025-01-05 11:33:23 -0800

    update

--- frontend/src/Pages/contacts/Contact.jsx
+++ frontend/src/Pages/contacts/Contact.jsx
@@ -40,7 +40,7 @@ function Contact() {
         <div className='mt-3 space-y-2'>
       <span>Name</span>
       <br />
-      <input type="text" className='py-1 rounded border outline-none dark:bg-slate-600 dark:text-white ' placeholder='Enter your name'
+      <input type="text" className='p-1 rounded border outline-none dark:bg-slate-600 dark:text-white ' placeholder='Enter your name'
       {...register("name",{required:true})}
       />
       <br />
@@ -49,7 +49,7 @@ function Contact() {
       <div className='mt-3 space-y-2'>
       <span>Email</span>
       <br />
-      <input type="email" className='py-1 rounded border outline-none dark:bg-slate-600 dark:text-white ' placeholder='Enter your email'
+      <input type="email" className='p-1  rounded border outline-none dark:bg-slate-600 dark:text-white ' placeholder='Enter your email'
        {...register("email",{required:true})}
       />
       <br />
@@ -58,7 +58,7 @@ function Contact() {
       <div className='mt-3 space-y-2'>
       <span>phone</span>
       <br />
-      <input type="text" className='py-1 rounded border outline-none dark:bg-slate-600 dark:text-white ' placeholder='Enter your number'
+      <input type="text" className='p-1 rounded border outline-none dark:bg-slate-600 dark:text-white ' placeholder='Enter your number'
       {...register("phone",{required:true})}
       />
       <br />
@@ -67,7 +67,7 @@ function Contact() {
       <div className='mt-3 space-y-2'>
       <span>Message</span>
       <br />
-      <textarea className='py-1 px-5 rounded border outline-none dark:bg-slate-600 dark:text-white ' placeholder='Enter your name'
+      <textarea className='w-full px-4 py-2 h-20 rounded border outline-none dark:bg-slate-600 dark:text-white ' placeholder='Enter your message'
        {...register("message",{required:true})}
       />
       <br />
